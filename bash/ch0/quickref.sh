701de5a5ef7d8bc2f4b477d5a6ba4db2a37c237f
giovan48 <giovan48@hotmail.com>
2026-05-09 15:05:19 -0700

    Update

--- bash/ch0/quickref.sh
+++ bash/ch0/quickref.sh
@@ -1,1 +1,36 @@
 #!/bin/bash
+#Cybersecurity Ops with bash
+
+#1- grep -> search the contents of files for a specific pattern
+    -c #count the number of lines that match the pattern
+    -E #use extended regular expressions for more complex pattern matching
+    -i #ignore case when searching for the pattern
+    -P #use Perl-compatible regular expressions for advanced pattern matching
+    -R #search recursively through directories for the pattern
+    -v #invert the match to find lines that do not match the pattern
+    -w #match the whole word only
+    -x #match the whole line only
+    -n #display the line number of each match
+    -H #display the filename for each match
+    -l #display only the filenames that contain the pattern
+    -L #display only the filenames that do not contain the pattern
+#2- find -> search the system for files
+    -exec #execute specified command for each file found
+    -name #search by filename
+    -size #search by file size
+    -type #search by file type
+#3- file
+#4- cut
+#5- head
+#6-tail
+#7- sort
+#8- xxd
+#9- wevtutil
+#10- tr
+#11- vi commands
+#12- curl
+#13- uniq
+#14- join
+#15- sdiff
+#16- base64
+#17- Regular expressions
